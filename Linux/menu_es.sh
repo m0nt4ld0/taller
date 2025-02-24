27264b734c90d924ca436c50f996402e3b3bbf20
m0nt4ld0 <mmontaldo@live.com>
2025-02-24 16:02:21 -0300

    Add version en ingles y espaniol del menu ppal

--- Linux/menu_es.sh
+++ Linux/menu_es.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Banner ASCII
 clear
 echo "======================================="
-echo "   WELCOME TO m0nt4ld0's WORKSHOP      "
+echo "   BIENVENIDOS AL TALLER DE m0nt4ld0   "
 echo "======================================="
 echo "               ____        __  __ __  __    ______  "
 echo "    ____ ___  / __ \____  / /_/ // / / /___/ / __ \ "
@@ -18,45 +18,44 @@ echo "github.com/m0nt4ld0"
 
 while true; do
     echo ""
-    echo "1) Detect magnet links in a webpage"
-    echo "2) Detect .torrent files in a webpage"
-    echo "3) Download ROMs from Retrostic.com"
-    echo "4) Cambiar a menú en español"
-    echo "0) Exit"
-    echo -n "Select an option: "
+    echo "1) Detectar enlaces magnet en una pagina web"
+    echo "2) Detectar archivos .torrent en una pagina web"
+    echo "3) Bajar Juegos de Retrostic.com"
+    echo "4) Switch to english menu"
+    echo "0) Salir"
+    echo -n "Seleccione una opción: "
     read -r opcion
 
     case $opcion in
         1)
-            echo "Enter the URL of the webpage where you want to detect magnet links: "
+            echo "Ingrese la URL de la página donde desea detectar Magnet Links: "
             read -r enlace
-            echo "Detecting magnet links on $enlace..."
+            echo "Detectando enlaces magnet en la página web $enlace..."
             bash "$SCRIPT_DIR/Linux/getMagnetLinks.sh" "$enlace"
             ;;
         2)
-            echo "Enter the URL of the webpage where you want to detect .torrent files: "
+            echo "Ingrese la URL de la página donde desea detectar archivos Torrent: "
             read -r url
-            echo "Detecting .torrent files on $url..."
+            echo "Detectando archivos .torrent en la página $url..."
             bash "$SCRIPT_DIR/Linux/getTorrentsLinks.sh" "$url"
             ;;
         3)
-            echo "Enter the console name followed by a space and the page number to download from Retrostic: "
+            echo "Ingrese la consola seguida por un espacio y el número de página para bajar juegos de Retrostic: "
             read -r consola pagina
-            echo "Downloading games for $consola on page $pagina..."
+            echo "Descargando juegos para la consola $consola en la página $pagina..."
             SCRIPT_DIR="$(dirname "$(realpath "$0")")"
             PYTHON_SCRIPT="$SCRIPT_DIR/Linux/bajarjuegos.py"
             python3 "$PYTHON_SCRIPT" "$consola" "$pagina"
             ;;
         4)
-            bash "$SCRIPT_DIR/Linux/menu_es.sh"
-            exit 0
+            bash "$SCRIPT_DIR/Linux/menu_en.sh"
             ;;
         0)
-            echo "See ya!"
+            echo "¡Hasta pronto!"
             exit 0
             ;;
         *)
-            echo "Invalid option. Try again."
+            echo "Opción no válida. Intente nuevamente."
             ;;
     esac
 done
